484f69e8244f0b51097523db741eeb70bf2a158e
NUKEDO <nukedo.4444.orz@gmail.com>
2020-11-13 18:25:43 +0900

    devise導入〜ストロングパラメーター設定まで
    Gem devise導入
    rails db:migrate
    rails g devise:views
    devise_parameter_sanitizer記述

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :gender_id
+    validates :height
+    validates :diet_reason
+  end
+
+end
