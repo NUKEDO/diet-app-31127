8740b47c1185c0ca7be5714e0db5abf0fcd2ee0d
NUKEDO <nukedo.4444.orz@gmail.com>
2020-11-17 11:32:47 +0900

    バリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  VALID_PASSWORD = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d{6,}]+\z/i.freeze
+  
   with_options presence: true do
-    validates :nickname
-    validates :birthday
-    validates :gender
-    validates :height
-    validates :diet_reason
+    validates :password,    format: { with: VALID_PASSWORD }
+    validates :nickname,    length: { maximum: 40 }
+    validates :birthday,    presence: { message: "Select" }
+    validates :gender,      inclusion: { in: ["male", "female"], message: "Select" }
+    validates :height,      numericality: { only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 300, message: "Enter in centimeters" }
+    validates :diet_reason, length: { maximum: 1024 }
   end
 
 end
