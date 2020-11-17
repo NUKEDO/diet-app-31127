6207cfd58975ff69c32c66b1db018bf130c1a164
NUKEDO <nukedo.4444.orz@gmail.com>
2020-11-17 19:21:38 +0900

    methodsコントローラーとルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'records#index'
   resources :records, only: :index
+  resources :methods, only: [:new, :create]
 end
---
+++ spec/requests/methods_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Methods", type: :request do
+
+end
