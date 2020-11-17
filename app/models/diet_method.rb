1807272607e97c89452b32e187ae8c7b5174c106
NUKEDO <nukedo.4444.orz@gmail.com>
2020-11-17 18:07:51 +0900

    ActiveHash仮作成

--- app/models/diet_method.rb
+++ app/models/diet_method.rb
@@ -1,2 +1,12 @@
-class DietMethod < ApplicationRecord
+class DietMethod < ActiveHash::base
+  self.data = [
+    { id: 1, name: '—-' }
+    { id: 2, name: 'カロリー制限(置き換えダイエットなど)' }
+    { id: 3, name: '運動・フィットネス・ストレッチ' }
+    { id: 4, name: 'サプリメント' }
+    { id: 5, name: '生活習慣' }
+    { id: 6, name: '食事制限(糖質制限など)' }
+    { id: 7, name: 'その他' }
+    
+  ]
 end
