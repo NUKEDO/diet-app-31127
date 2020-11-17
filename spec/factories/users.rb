c5ac454ad7e9a7c496124529bf55d1a3c2f842ac
NUKEDO <nukedo.4444.orz@gmail.com>
2020-11-17 12:25:36 +0900

    単体テストコード作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    email                 {Faker::Internet.free_email}
+    password              {'123xyz'}
+    password_confirmation {password}
+    nickname              {Faker::Name.initials(number: 2)}
+    birthday              {'2000-03-24'}
+    gender                {"female"}
+    height                {180}
+    diet_reason           {"モテるため"}
   end
 end
