d357790d0ea30fe11d1909c9d6f7422d9c3480fa
NUKEDO <nukedo.4444.orz@gmail.com>
2020-11-19 19:29:56 +0900

    ActiveHash用モデル作成、記述

--- app/models/diet_method.rb
+++ app/models/diet_method.rb
@@ -3,7 +3,11 @@ class DietMethod < ApplicationRecord
     validates :title
     validates :type_id
     validates :explanation
+    validates :genre_id,    numerically: { other_than: 1 }
   end
 
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :type
 end
---
+++ app/models/type.rb
@@ -0,0 +1,14 @@
+class Type < ActiveHash::base
+  self.data = [
+    { id: 1, name: '--' }
+    { id: 2, name: '食事制限' }
+    { id: 3, name: '運動' }
+    { id: 4, name: 'サプリメント等' }
+    { id: 5, name: '生活習慣変更' }
+    { id: 6, name: 'その他：摂取カロリーを減らす' }
+    { id: 7, name: 'その他：消費カロリーを増やす' }
+  ]
+
+  include ActiveHash::Associations 
+  has_many :diet_method
+end
