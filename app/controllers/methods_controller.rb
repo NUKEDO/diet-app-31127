1873a03fce842e2772fbf81e20b9c47451ce8f78
NUKEDO <nukedo.4444.orz@gmail.com>
2020-11-19 18:19:44 +0900

    ストロングパラメーター作成
    authenticate_user!も記述

--- app/controllers/methods_controller.rb
+++ app/controllers/methods_controller.rb
@@ -1,4 +1,6 @@
 class MethodsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+  
   def new
     @diet_method = DietMethod.new
   end
@@ -10,6 +12,6 @@ class MethodsController < ApplicationController
   private
 
   def diet_method_params
-    params.require(:diet_method).permit().merge(user_id: current_user.id)
+    params.require(:diet_method).permit(:title, :type_id, :explanation, :ease_of_reduction, :ease_of_continuation).merge(user_id: current_user.id)
   end
 end
